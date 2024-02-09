f32347003a0c0a5376c7c03b9c6e636903609518
GargDivanshu <divanshu.garg113@gmail.com>
2024-02-09 21:29:32 +0530

    sol checking

---
+++ src/components/SumCalculator.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+
+
+const SumCalculator = () => {
+ const [numbers, setNumbers] = useState([]);
+ const [sum, setSum] = useState(0);
+
+ useEffect(() => {
+   setSum(numbers.reduce((acc, number) => acc + number, 0));
+ }, [numbers]);
+
+
+ const handleNumberInput = (event) => {
+   const number = parseInt(event.target.value);
+   setNumbers((prevNumbers) => [...prevNumbers, number]);
+ };
+
+
+ return (
+   <div>
+     <h1>Sum Calculator</h1>
+     <input type="number" onChange={handleNumberInput} />
+     <p>Sum: {sum}</p>
+   </div>
+ );
+};
+
+
+export default SumCalculator;
